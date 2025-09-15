60f901cdb3f98c1ae9effee2c9a45ae1a3422143
Sowndhar M <sowndharsowndhar888@gmail.com>
2025-09-15 15:50:52 +0530

    Update Firbase.js

--- src/Component/Config/Firbase.js
+++ src/Component/Config/Firbase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeLjwemrn9WPS1p-fJfxyXyPWVpEWJ6Z0",
-  authDomain: "react-d52d5.firebaseapp.com",
-  projectId: "react-d52d5",
-  storageBucket: "react-d52d5.firebasestorage.app",
-  messagingSenderId: "366978697304",
-  appId: "1:366978697304:web:44e788c221970308d9222d"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
